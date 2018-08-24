9c2502697b439ea1f4b9c1b03e4f82794153c032
jang032 <jang_032@naver.com>
2018-08-24 17:24:12 +0900

    ㅅㄱ

---
+++ GMS/src/main/java/com/goods/app/LoginController.java
@@ -0,0 +1,14 @@
+package com.goods.app;
+
+public class LoginController 
+{
+	
+	public void logout(){
+		System.out.println("연봉많이주세요");
+	}
+	
+	
+	public void login(){
+		System.out.println("좀 되세요");
+	}
+}
