b884fa859670d04993ebd055b4518318cdc422f9
park0 <park0@DESKTOP-UH0G566>
2018-08-25 12:09:29 +0900

    first master

--- GMS/src/main/java/com/goods/app/vo/MemberVO.java
+++ GMS/src/main/java/com/goods/app/vo/MemberVO.java
@@ -2,8 +2,5 @@ package com.goods.app.vo;
 
 public class MemberVO {
 	
-	public static void aa() {
-		System.out.println("gogo");
-	}
-	
+
 }
--- GMS/src/main/java/com/goods/service/LoginService.java
+++
@@ -1,6 +0,0 @@
-package com.goods.service;
-
-public class LoginService 
-{
-
-}
